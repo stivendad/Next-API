9519f763c6252fde5cbe2ebe04600c65be405213
Daniel Tejada <dastte93@gmail.com>
2024-03-14 14:27:03 -0500

    Login y Logout implementados

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
-import { SidebarItem } from "@/components"
+import { LogoutButton, SidebarItem } from "@/components"
 import { IoBasketOutline, IoCalendarOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline, IoPersonCircleOutline } from "react-icons/io5";
 import { CiLogout } from "react-icons/ci";
 import { getServerSession } from "next-auth";
@@ -89,10 +89,7 @@ export const Sidebar = async () => {
             </div>
 
             <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
-                <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-                    <CiLogout />
-                    <span className="group-hover:text-gray-700">Logout</span>
-                </button>
+                <LogoutButton />
             </div>
         </aside>
     )
